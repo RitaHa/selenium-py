d7cd2960cb044c866481fd9058771976713b5c4b
Rita Hancharenka <ritahancharenka@Ritas-MacBook-Pro.local>
2024-01-19 17:51:48 +0100

    Add parametrized test

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,21 +1,27 @@
+import pytest
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
 from pages.result import GoogleResultPage
 from pages.search import GoogleSearchPage
 
 
-def test_google_search(browser):
+@pytest.mark.parametrize('phrase', ['Carbonara', 'Mascarpone', 'Arancini'])
+def test_google_search(browser, phrase):
     search_page = GoogleSearchPage(browser)
     result_page = GoogleResultPage(browser)
-    phrase = "Carbonara"
     #     Given the Google home page is displayed
     search_page.open()
     #     When the user close Cookies popup
     assert search_page.close_cookies_popup() == False
-    #     When the user searches for "Carbonara"
+    #     When the user searches for phrase
     search_page.search(phrase)
-    #     Then the search result title contains "Carbonara"
+    #     Then the search result title contains phrase
+    # WebDriverWait(browser, 10).until(expected_conditions.title_contains(phrase))
+    # don't Implicit Wait vs Explicit Wait
     assert phrase in result_page.title()
-    #     And the search result query is "Carbonara"
+    #     And the search result query is phrase
     assert phrase == result_page.search_input_value()
-    #     And the search result links pertain to "Carbonara"
+    #     And the search result links pertain to phrase
     for title in result_page.result_link_titles():
         assert phrase.lower() in title.lower()
